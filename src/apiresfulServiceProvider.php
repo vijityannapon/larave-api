e334679bf97ebe40879efaaf41d97d3802f43e54
V.Akekachai <vijityannapon@gmail.com>
2017-08-03 09:37:40 +0000

    first commit

---
+++ src/apiresfulServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace vijityannapon\apiresful;
+
+use Illuminate\Support\ServiceProvider;
+
+class apiresfulServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
